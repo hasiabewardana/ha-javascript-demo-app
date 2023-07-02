4a854242513dfd14abaea381706de15b180f0b0d
hasiabewardana <hasi.prasanga@gmail.com>
2023-07-02 11:41:52 +0530

    Implemented multiple exports and imports.

---
+++ assets/scripts/app.js
@@ -0,0 +1,10 @@
+import message, {apiKey as key, apiSecret as sec} from "./util.js";
+// import * as util from "./util.js"
+
+console.log(message)
+console.log(key);
+console.log(sec);
+
+// console.log(util.default)
+// console.log(util.apiKey);
+// console.log(util.apiSecret);
---
+++ assets/scripts/util.js
@@ -0,0 +1,3 @@
+export default "Hello World!"
+export let apiKey = 'hcechekcjec';
+export let apiSecret = 'ecencjelcnec';
